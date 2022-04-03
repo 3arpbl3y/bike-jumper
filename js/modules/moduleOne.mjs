58e52ca5476dbf8fbee4d2f916132641028ace54
3arpbl3y <3arpbl3y@gmail.com>
2022-04-03 22:31:42 +0500

    removed unnecessery files

--- js/modules/moduleOne.mjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const sum = (a, b) => a + b;
-
-const mult = (a, b) => a * b;
-
-export { sum, mult };
--- js/modules/moduleTwo.mjs
+++
@@ -1,4 +0,0 @@
-import { sum, mult as syxx } from "./moduleOne.mjs";
-
-console.log(sum(5, 5));
-console.log(syxx(5, 5));
